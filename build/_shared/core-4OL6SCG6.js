4a00c4a779475ff7a194582f954b8882004d0dd7
Maciej Skorski <maciej.skorski@gmail.com>
2026-03-02 22:02:53 +0100

    Update documentation

--- build/_shared/core-4OL6SCG6.js
+++ build/_shared/core-4OL6SCG6.js
@@ -1,1 +1,1 @@
-import{a}from"/build/_shared/chunk-ZW3ZE2TT.js";import"/build/_shared/chunk-TFZDQLJI.js";import"/build/_shared/chunk-3FJ63ARC.js";import"/build/_shared/chunk-KKLOHGIO.js";import"/build/_shared/chunk-I2P5JYEV.js";import"/build/_shared/chunk-OZE3FFNP.js";export default a();
+import{a}from"/KDDM/build/_shared/chunk-ZW3ZE2TT.js";import"/KDDM/build/_shared/chunk-TFZDQLJI.js";import"/KDDM/build/_shared/chunk-3FJ63ARC.js";import"/KDDM/build/_shared/chunk-KKLOHGIO.js";import"/KDDM/build/_shared/chunk-I2P5JYEV.js";import"/KDDM/build/_shared/chunk-OZE3FFNP.js";export default a();
--- build/_shared/velocity-NU2EDPZO.js
+++ build/_shared/velocity-NU2EDPZO.js
@@ -1,1 +1,1 @@
-import{a}from"/build/_shared/chunk-GICMYR45.js";import"/build/_shared/chunk-OZE3FFNP.js";export default a();
+import{a}from"/KDDM/build/_shared/chunk-GICMYR45.js";import"/KDDM/build/_shared/chunk-OZE3FFNP.js";export default a();
